af8004a67ca8dd1155faccccfde68d05680508c2
valem <valemk98@hotmail.com>
2020-02-06 03:57:34 +0100

    ac

--- LogicaNegocio/UsuarioLN.cs
+++ LogicaNegocio/UsuarioLN.cs
@@ -11,7 +11,7 @@ namespace LogicaNegocio
 {
     public class UsuarioLN
     {
-        //Método Crear usuario yoop
+        //Método Crear usuario yoopddd
         public static void crearUser(Usuario usuario)
         {
             UsuarioDatos.Insertar(usuario);
